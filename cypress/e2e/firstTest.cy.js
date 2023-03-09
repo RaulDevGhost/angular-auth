725e8fef99038f85272ed0d8f20c4901d9db28d0
rri <raul.rincones@adorsys.com>
2023-03-09 14:05:02 +0100

    new commands for my test file

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,6 +1,19 @@
 describe("First Test", () => {
-  it("Navigate to our first page", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:64192/");
-    cy.reload();
+  });
+  it("Frontend page can be opened / Smoke testing", () => {
+    cy.reload(true);
+    cy.contains("Log in to Trello");
+  });
+
+  it("Make the login", () => {
+    cy.get("[formControlName='email']")
+      .type("test@test.com")
+      .should("have.value", "test@test.com");
+    cy.get("[formControlName='password']")
+      .type("12345678")
+      .should("have.value", "12345678");
+    cy.get("#login-button-form").click();
   });
 });
